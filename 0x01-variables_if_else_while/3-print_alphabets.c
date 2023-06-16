85b163152c0a903569bb74a0734985b2e0ebb87d
ajirogbagoma <oghenetegaangela@gmail.com>
2023-06-16 14:40:36 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char e;
+
+	char h;
+
+	e = 'g';
+	h = 'G';
+	while
+		(e <= 'g') {
+			putchar(e);
+			e++;
+		}
+	while
+		(h <= 'G') {
+			putchar (h);
+			h++;
+		}
+	putchar('\n');
+	return (0);
+
+}
